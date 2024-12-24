5ca83a5612f9233e166b23a4931604aa9546d564
Andrew <andrew-jung@hotmail.com>
2024-12-24 15:32:08 -0800

    Fix deploy errors

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ 
+    log: ['query', 'info', 'warn', 'error'],
 })
 
-export default prisma
+// This is critical in serverless environments to ensure a single instance
+if (process.env.NODE_ENV === "production") {
+  globalForPrisma.prisma = prisma
+}
