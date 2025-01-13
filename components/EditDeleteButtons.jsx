3b6666fdd7d91471b82a87710d712951a7dac4ac
Andrew <andrew-jung@hotmail.com>
2025-01-13 10:21:05 -0800

    Fix deploy errors

--- components/EditDeleteButtons.jsx
+++ components/EditDeleteButtons.jsx
@@ -47,29 +47,34 @@ const EditDeleteButtons = ({post}) => {
 
   return (
     <div>
-        <div className="flex-row flex justify-center items-center gap-3">
-            <Button  onClick={()=>editPost(post.id)}>
-                Edit
-            </Button>
-            <Button  onClick={()=>setDeleteModal(post)}>
-                Delete
-            </Button>
-        </div>
-        { deleteModal && (
-            <div onClick={()=>setDeleteModal(null)} className='fixed inset-0 bg-black bg-opacity-65 z-50 flex justify-center items-center'>
-                <div onClick={(e)=>e.stopPropagation()} className=' justify-center items-center flex flex-col w-96 h-60'>
-                    <Card >
-                        <CardContent>
-                            <p className='my-8'>Are you sure you want to delete the post: <strong>{deleteModal.title}?</strong></p>
-                            <div className="flex gap-3 items-center justify-center">
-                                <Button onClick={()=> setDeleteModal(null)}>Cancel</Button>
-                                <Button onClick={()=>deletePost(deleteModal.id)}>Delete</Button>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </div>
+        { session?.data?.user?.role === 'ADMIN' && (
+            <>
+
+            <div className="flex-row flex justify-center items-center gap-3">
+                <Button  onClick={()=>editPost(post.id)}>
+                    Edit
+                </Button>
+                <Button  onClick={()=>setDeleteModal(post)}>
+                    Delete
+                </Button>
             </div>
-        ) }
+            { deleteModal && (
+                <div onClick={()=>setDeleteModal(null)} className='fixed inset-0 bg-black bg-opacity-65 z-50 flex justify-center items-center'>
+                    <div onClick={(e)=>e.stopPropagation()} className=' justify-center items-center flex flex-col w-96 h-60'>
+                        <Card >
+                            <CardContent>
+                                <p className='my-8'>Are you sure you want to delete the post: <strong>{deleteModal.title}?</strong></p>
+                                <div className="flex gap-3 items-center justify-center">
+                                    <Button onClick={()=> setDeleteModal(null)}>Cancel</Button>
+                                    <Button onClick={()=>deletePost(deleteModal.id)}>Delete</Button>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            ) }
+            </>
+        )} 
     </div>
   )
 }
